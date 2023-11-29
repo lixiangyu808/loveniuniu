1f9d4d36549eb8e1e759a2b54d2b0ddf4baa6dd2
lixiangyu808 <49385880+lixiangyu808@users.noreply.github.com>
2023-11-29 11:32:44 +0800

    Update time.js

--- jscss/time.js
+++ jscss/time.js
@@ -1,6 +1,6 @@
 function showTime() {
 	var currentDate = new Date();
-	var startDate = new Date(2013, 9, 19);
+	var startDate = new Date(2020, 12, 5);
 	var date3 = currentDate-startDate;
 	var days=Math.floor(date3/(24*3600*1000));
 	var leave1=date3%(24*3600*1000);     //计算天数后剩余的毫秒数
@@ -13,8 +13,8 @@ function showTime() {
 	minutes = "0" + minutes;
 	if (seconds < 10)
 	seconds = "0" + seconds;
-	var currentTimeString = "Dear Nan:<br>"
-		+ "I have been in love with you for:<br> "
+	var currentTimeString = "亲爱的小妞妞:<br>"
+		+ "已经和李老公在一起生活了:<br> "
 		+ "&nbsp&nbsp&nbsp&nbsp&nbsp"
 		+ "<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" + days +" </c>day "
 		+ "<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" + hours+" </c>hour "
@@ -22,7 +22,7 @@ function showTime() {
 		+"<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" +seconds+" </c>sec<br>"
 		+ "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp执子之手,与子偕老.<br>"
 		+ "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"
-		+	"<c style=\"color: #CCCCCC; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:20px\" >----Hao   2018-10-19</c>";
+		+	"<c style=\"color: #CCCCCC; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:20px\" >----李老公   2023-11-29</c>";
 	document.getElementById("show").innerHTML=currentTimeString;  //改这地方
 	window.setTimeout("showTime()", 1000);
 }
